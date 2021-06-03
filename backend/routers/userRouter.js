b5649e440935af475beed94320b9c95706088733
bereole <66678029+bereole@users.noreply.github.com>
2021-06-03 17:15:00 +0000

    Add files via upload

---
+++ backend/routers/userRouter.js
@@ -0,0 +1,78 @@
+import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import bcrypt from 'bcryptjs';
+import data from '../data.js';
+import User from '../models/userModel.js';
+import { generateToken } from '../utils.js';
+
+const userRouter = express.Router();
+
+userRouter.get(
+    '/seed', 
+    expressAsyncHandler(async (req, res) => {
+        // await User.remove({});
+  const createdUsers = await User.insertMany(data.users);
+  res.send({ createdUsers });
+    }) 
+);
+
+userRouter.post('/signin', expressAsyncHandler( async(req, res) => {
+    const user = await User.findOne({email: req.body.email});
+    if (user){
+        if(bcrypt.compareSync(req.body.password, user.password)){
+            res.send({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                isSponsor: user.isSponsor,
+                isVendor: user.isVendor,
+                token: generateToken(user),
+            });
+            return;
+        }
+    }
+    res.status(401).send({message: "Invalid user email or Password"});
+ })
+);
+
+userRouter.post('/sregister', expressAsyncHandler(async(req, res) => {
+    const user = new User({
+        name: req.body.name, 
+        email: req.body.email,  
+        isSponsor: req.body.isSponsor || true,
+        password: bcrypt.hashSync(req.body.password, 8),
+    });
+    const createdUser = await user.save();
+    res.send({
+        _id: createdUser._id,
+        name: createdUser.name,
+        email: createdUser.email,
+        isAdmin: createdUser.isAdmin,
+        isSponsor: createdUser.isSponsor,
+        isVendor: createdUser.isVendor,
+        token: generateToken(createdUser),
+    });
+ })
+);
+
+userRouter.post('/vregister', expressAsyncHandler(async(req, res) => {
+    const user = new User({
+        name: req.body.name, 
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 8),
+    });
+    const createdUser = await user.save();
+    res.send({
+        _id: createdUser._id,
+        name: createdUser.name,
+        email: createdUser.email,
+        isAdmin: createdUser.isAdmin,
+        isSponsor: createdUser.isSponsor,
+        isVendor: createdUser.isVendor,
+        token: generateToken(createdUser),
+    });
+ })
+);
+
+export default userRouter;
